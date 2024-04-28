05c98229687288e4d20e18f69f901a2e5c1004b1
harshilbhatt09 <bhattharshu9503@gmail.com>
2024-04-28 14:42:06 +0530

    updates

--- data.php
+++ data.php
@@ -8,23 +8,28 @@ $conn = mysqli_connect($server, $username, $password, $dbname);
 if (!$conn) {
     die("connection to database failed due to: " . mysqli_connect_error());
 }
-echo("connection succ");
-// $en_no = $_POST['en_no'];
-// $fname = $_POST['fname'];
-// $lname = $_POST['lname'];
-// $toc = $_POST['toc'];
-// $wp = $_POST['wp'];
-// $dv = $_POST['dv'];
+echo ("connection succ");
+$fullname = $_POST['fullname'];
+$dob = $_POST['dob'];
+$blood = $_POST['group'];
+$district = $_POST['district'];
+$state = $_POST['state'];
+$nation = $_POST['nation'];
+$height = $_POST['height'];
+$weight = $_POST['weight'];
+$gender = $_POST['gender'];
 // if ($_REQUEST['btn'] == "insert") {
-//     $sql = "INSERT INTO marksheet VALUES ('$en_no','$fname', '$lname','$toc','$wp','$dv');";
-//     if (mysqli_query($conn, "$sql")) {
-//         echo ("Succesfully inserted");
-//     } else {
-//         echo ("ERROR: $sql <br> $conn->error");
-//     }
-// } else {
+$sql = "INSERT INTO blood (FULL_NAME, DOB, BLOODGROUP, DISTRICT, STATE, NATION, HEIGHT, WEIGHT, GENDER) VALUES ('$fullname', '$dob', '$blood', '$district', '$state', '$nation', '$height', '$weight', '$gender');";
+if (mysqli_query($conn, "$sql")) {
+    header("Location: http://localhost/Kshat_Suraksha/");
+    exit;
+} else {
+    echo ("ERROR: $sql <br> $conn->error");
+}
+// } 
+// else {
 //     echo ("ERROR: $sql <br> $conn->error");
 // }
-// $conn->close();
+$conn->close();
 
 ?>
